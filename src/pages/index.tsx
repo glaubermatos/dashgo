8d6e0e4df411080db6701b4914e20d7eb40fca2a
glaubermatos <glaub.oliveira@hotmail.com>
2022-04-29 22:18:16 -0300

    Dashboard page, create Header and Sidebar component, style apexchart

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
-import { Flex, Input, Button, Stack, FormLabel, FormControl } from '@chakra-ui/react'
+import { Flex, Button, Stack, } from '@chakra-ui/react'
+import { Input } from '../components/Form/Input'
 
-
-export default function Home() {
+export default function SignIn() {
   return (
     <Flex
       w="100vw"
@@ -20,48 +20,18 @@ export default function Home() {
         flexDirection="column"
       >
         <Stack spacing="4">
-
-          <FormControl>
-            <FormLabel htmlFor="email">E-mail</FormLabel>
-            <Input
-              id="email"
-              name="email"
-              type="email" 
-              focusBorderColor="pink.500"
-              bgColor="gray.900"
-              variant="filled"
-              _hover={{
-                bgColor: 'gray.900'
-              }}
-              size="lg"
-            />
-          </FormControl>
-
-          <FormControl>
-            <FormLabel htmlFor="password">Password</FormLabel>
-            <Input
-              id="password"
-              name="password"
-              type="password" 
-              focusBorderColor="pink.500"
-              bgColor="gray.900"
-              variant="filled"
-              _hover={{
-                bgColor: 'gray.900'
-              }}
-              size="lg"
-            />
-          </FormControl>
+          <Input name='email' label='E-mail' type='email' />
+          <Input name='password' label='Password' type='password' />
         </Stack>
 
-       <Button
-          type="submit"
-          mt="6"
-          colorScheme="pink"
-          size="lg"
-        >
-          Entrar
-        </Button>
+        <Button
+            type="submit"
+            mt="6"
+            colorScheme="pink"
+            size="lg"
+          >
+            Entrar
+          </Button>
       </Flex>
     </Flex>
   )
