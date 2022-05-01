a7c3d26ad2c89d022e02f1052b50fa672cfcd444
glaubermatos <glaub.oliveira@hotmail.com>
2022-05-01 20:58:06 -0300

    responsive sidebar with Drawer menu

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,6 @@
-import { Flex, useBreakpointValue } from '@chakra-ui/react'
+import { Button, Flex, Icon, IconButton, useBreakpointValue } from '@chakra-ui/react'
+import { RiMenuLine } from 'react-icons/ri'
+import { useSidebarDrawer } from '../../contexts/SidebarDraweContext'
 
 import { Logo } from './Logo'
 import { NotificationsNav } from './NotificationsNav'
@@ -6,6 +8,8 @@ import { Profile } from './Profile'
 import { SearchBox } from './SearchBox'
 
 export function Header() {
+
+    const {onOpen} = useSidebarDrawer()
 
     const isWideVersion = useBreakpointValue({
         base: false,
@@ -21,8 +25,20 @@ export function Header() {
           marginX="auto"
           marginTop="4"
           paddingX="6"
-          align="center"
+          align="center"          
         >
+
+            { !isWideVersion && (
+                <IconButton
+                    aria-label="Open navigation"
+                    icon={<Icon as={RiMenuLine} />}
+                    fontSize="24"
+                    variant="unstyled"
+                    onClick={onOpen}
+                    mr="2"
+                />
+            )}
+
             <Logo  />
 
             { isWideVersion && ( <SearchBox /> )}
