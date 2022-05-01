6c0c01e6dbb99b17584e6d9118b166d35ae35acb
glaubermatos <glaub.oliveira@hotmail.com>
2022-05-01 08:54:24 -0300

    responsive header

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,18 +1,23 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+    showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
     return(
         <Flex align="center" >
-            <Box mr="4" textAlign="right">
-                <Text>Glauber Matos</Text>
-                <Text
-                    color="gray.300"
-                    fontSize="small"
-                >
-                    glaub.oliveira@hotmail.com
-                </Text>
-
-            </Box>
+            { showProfileData && (
+                <Box mr="4" textAlign="right">
+                    <Text>Glauber Matos</Text>
+                    <Text
+                        color="gray.300"
+                        fontSize="small"
+                    >
+                        glaub.oliveira@hotmail.com
+                    </Text>
+                </Box>
+            ) }
 
             <Avatar size="md" name="Glauber Matos" src="https://github.com/glaubermatos.png" />
         </Flex>
