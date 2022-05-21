81554ce25ee6e275a32b2754d70523821614c1ae
glaubermatos <glaub.oliveira@hotmail.com>
2022-05-21 06:51:48 -0300

    configuring axios

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -7,6 +7,7 @@ import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
 import { useQuery } from 'react-query'
+import { api } from "../../services/api";
 
 type User = {
     id: string;
@@ -18,8 +19,7 @@ type User = {
 
 export default function UserList() {
     const { data, isLoading, isFetching, error } = useQuery('users', async () => {
-        const response = await fetch('http://localhost:3000/api/users')
-        const data = await response.json()
+        const { data } = await api.get('users')
 
         const users = data.users.map((user) => {
             return {
