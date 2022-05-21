b3f93639af9d6cf9cf17f15ac535e2cb2bd9cdfc
glaubermatos <glaub.oliveira@hotmail.com>
2022-05-21 07:30:10 -0300

    extending miragejs functionality with data pagination

--- src/services/mirage/index.ts
+++ src/services/mirage/index.ts
@@ -1,4 +1,4 @@
-import { createServer, Factory, Model } from 'miragejs'
+import { createServer, Factory, Model, Response, RestSerializer } from 'miragejs'
 import faker from '@faker-js/faker'
 
 type User = {
@@ -13,6 +13,13 @@ export function makeServer() {
         models: {
             user: Model.extend<Partial<User>>({})
         },
+
+        // serializers: {
+        //     user: RestSerializer.extend({
+        //         include: ["list"],
+        //         embed: true,
+        //     }),
+        // },
 
         factories: {
             user: Factory.extend({
@@ -29,14 +36,31 @@ export function makeServer() {
         },
 
         seeds(server) {
-            server.createList('user', 10)
+            server.createList('user', 200)
         },
     
         routes() {
             this.namespace = 'api'
             this.timing = 750
 
-            this.get('/users')
+            this.get('/users', function (schema, request) {
+                const { page = 1, per_page = 10 } = request.queryParams
+
+                const total = schema.all('user').length
+
+                const pageStart = (Number(page) - 1) * Number(per_page)
+                const pageEnd = pageStart + Number(per_page)
+
+                const users = this.serialize(schema.all('user')).users.slice(pageStart, pageEnd)
+
+                return new Response(
+                    200,
+                    { 'x-total-count': String(total) },
+                    { users }
+                )
+                
+            })
+            
             this.post('/users')
     
             this.namespace = ''
